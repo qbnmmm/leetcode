b07c39a4f1ddea1a2c436dc508278990305aa32d
qiubingnan <qbnmmm@qq.com>
2022-08-09 22:51:59 +0800

    220809

---
+++ 动态规划/10. 正则表达式匹配.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s) + 1, len(p) + 1
+
+        # dp[i][j]表示s的前i个字符和p的前j个字符能否匹配
+        dp = [[False] * n for _ in range(m)]
+
+        # 初始化，s此时为空字符串
+        dp[0][0] = True  # 空字符串
+        for j in range(2, n, 2):  # p的偶数位为*则能匹配
+            dp[0][j] = dp[0][j - 2] and p[j - 1] == '*'
+
+        '''
+        p[i - 1]为'*'时，dp[i][j]为以下任意情况则为True：
+            1、dp[i][j - 2]:将字符串组合p[j - 2]*看作出现0次，能否匹配；
+            2、dp[i - 1][j] and s[i - 1] == p[j - 2]:让字符p[j - 2]多出现一次，能否匹配；
+            3、dp[i - 1][j] and p[j - 2] == '.':让字符'.'多出现一次，能否匹配。
+        p[j - 1]不为'*'时，dp[i][j]为以下任意情况则为True：
+            1、dp[i - 1][j - 1] and s[i - 1] == p[j - 1]:字符串 s 的前 i-1 个字符和 p 的前 j -1个字符匹配, 且s的第i个字符等于p的第j个字符；
+            2、dp[i - 1][j - 1] and p[i - ] == '.':字符串 s 的前 i-1 个字符和 p 的前 j -1个字符匹配, 且p的第j个字符为'.'。
+        '''
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j - 2] or dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.') \
+                    if p[j - 1] == '*' else \
+                    dp[i - 1][j - 1] and (p[j - 1] == '.' or s[i - 1] == p[j - 1])
+        return dp[-1][-1]
--- 数学/4. 寻找两个正序数组的中位数.py
+++ 数学/4. 寻找两个正序数组的中位数.py
@@ -1,8 +1,9 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        # 如果总共有奇数个元素，x和y相等
         x = (len(nums1) + len(nums2) - 1) // 2
         y = (len(nums1) + len(nums2)) // 2
-        i, j, med = 0, 0, 0
+        i, j, med = 0, 0, 0  # i, j分别指向nums1,nums2
         while i + j <= y:
             if i < len(nums1) and j < len(nums2):
                 if nums1[i] < nums2[j]:
@@ -11,16 +12,16 @@ class Solution:
                 else:
                     mid = nums2[j]
                     j += 1
-            elif i < len(nums1):
+            elif i < len(nums1):  # nums2全在前半段
                 mid = nums1[i]
                 i += 1
-            elif j < len(nums2):
+            elif j < len(nums2):  # nums1全在前半段
                 mid = nums2[j]
                 j += 1
 
-            if i + j > x:
+            if i + j > x:  # 如果有偶数个元素，med就是两数之和
                 med += mid
 
-        if x != y:
+        if x != y:  # 总共偶数个元素
             med /= 2
         return med
