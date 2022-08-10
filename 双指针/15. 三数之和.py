63f3339bcbe888fd9579cd037f352e3c1229500e
qiubingnan <qbnmmm@qq.com>
2022-08-10 16:47:11 +0800

    220810

--- 双指针/15. 三数之和.py
+++ 双指针/15. 三数之和.py
@@ -2,7 +2,7 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         res, k = [], 0
-        for k in range(len(nums) - 2):
+        for k in range(len(nums) - 2):  # 固定k，i和j分别指向数组两侧向中间搜索
             if nums[k] > 0: break
             if k > 0 and nums[k] == nums[k - 1]: continue # 相同元素跳过
             i, j = k + 1, len(nums) - 1
