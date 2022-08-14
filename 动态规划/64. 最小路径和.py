f2a62d90c0a8a569fc784ddc1da96c452896a3f1
qiubingnan <qbnmmm@qq.com>
2022-08-14 22:43:27 +0800

    220814

--- 动态规划/64. 最小路径和.py
+++ 动态规划/64. 最小路径和.py
@@ -1,13 +1,16 @@
 class Solution:
     def minPathSum(self, grid: list[list[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [[0] * n for _ in range(m)]
+        dp = [[0] * n for _ in range(m)]  # dp[i][j]表示从起点到（i, j)的最小路径和
         dp[0][0] = grid[0][0]
+
+        # 初始化：第一行和第一列
         for i in range(1, n):
             dp[0][i] = dp[0][i - 1] + grid[0][i]
         for i in range(1, m):
             dp[i][0] = dp[i - 1][0] + grid[i][0]
 
+        # 当前格子只会从上方和左方到达，进一步降低空间复杂度可以用滚动数组
         for i in range(1, m):
             for j in range(1, n):
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
--- 动态规划/72. 编辑距离.py
+++ 动态规划/72. 编辑距离.py
@@ -1,12 +1,23 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
+
+        # dp[i][j] 表示word1的前i个字符和word2的前j个字符的距离
         dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # 初始化：任意空字符串与另一个单词的距离
         for i in range(1, n + 1):
             dp[0][i] = dp[0][i - 1] + 1
         for i in range(1, m + 1):
             dp[i][0] = dp[i - 1][0] + 1
 
+        '''
+        状态转移：
+        增:dp[i][j] = dp[i][j - 1] + 1
+        删:dp[i][j] = dp[i - 1][j] + 1
+        改:dp[i][j] = dp[i - 1][j - 1] + 1
+        取三种方式的最小值
+        '''
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
