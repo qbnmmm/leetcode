82c5df1ae12025ff522efd6f83190764f81679c6
qiubingnan <qbnmmm@qq.com>
2022-09-09 23:14:06 +0800

    220909

---
+++ 每日一题/220909_1598. 文件夹操作日志搜集器.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        ans = 0
+        for log in logs:
+            if log == "./":
+                continue
+            elif log == "../":
+                if ans > 0:
+                    ans -= 1
+            else:
+                ans += 1
+        return abs(ans)
