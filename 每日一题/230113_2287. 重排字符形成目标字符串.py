2f2c8b0f227efbb183d7c1c6b0e025e39a470a99
qbnmmm <qbnmmm@qq.com>
2023-01-13 11:25:22 +0800

    230113

---
+++ 每日一题/230113_2287. 重排字符形成目标字符串.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+
+class Solution:
+    def rearrangeCharacters(self, s: str, target: str) -> int:
+        cnt1 = Counter(s)
+        cnt2 = Counter(target)
+        return min(cnt1[c] // v for c, v in cnt2.items())
